c04b9ab5e3bd959025e807d05686d900b043caae
EternallyCold <eternallycold03@gmail.com>
2024-12-18 15:55:26 +0000

    addeed playermovement scripts and buttons

--- AlienGameUnity/Assets/Scripts/PlayerMovement.cs
+++ AlienGameUnity/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D physicsBody = null;
+
+    //awake is called before start
+    private void Awake()
     {
-        
+        physicsBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveLeft()
     {
-        
+        //physicsBody.velocity.x = -1;
+
+        //make a variable to hold our velocity
+        //get velocity from rigedbody
+
+        Vector2 newVelocity = physicsBody.velocity;
+
+        newVelocity.x = -1;
+
+        physicsBody.velocity = newVelocity;
+    }
+
+    public void MoveRight()
+    {
+        //physicsBody.velocity.x = 1;
+
+        //make a variable to hold our velocity
+        //get velocity from rigedbody
+
+        Vector2 newVelocity = physicsBody.velocity;
+
+        newVelocity.x = 1;
+
+        physicsBody.velocity = newVelocity;
     }
 }
